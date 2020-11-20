74333b6f08cabe49e82cdfca00da2bb625744220
Andrew <andrew.joseffer@gmail.com>
2020-11-20 11:25:09 -0500

    Intial html, css, and javascript files

---
+++ scripts.js
@@ -0,0 +1,52 @@
+// $(document).ready(function(){
+     
+//     //  Dark Mode  
+// $('.dark').on('click', function(){
+//     if($("#container").hasClass('darkMode')){
+//         $("#container").removeClass('darkMode');
+//         resetColors();
+//     }else{
+//         removeClasses();
+//         $("#container").addClass('darkMode');
+//         $('#container').css('background-color', 'black');
+//         $('#container').css('color', 'white');
+//     }
+// });
+
+//     // Reader Accessibility Mode
+// $('.reader_accessibility').on('click', function(){
+//     if($('#container').hasClass('raMode')){
+//         $('#container').removeClass('raMode');
+//         resetFont();
+//     }else{
+//         removeClasses();
+//         $('#container').addClass('raMode');
+//         $('#container').css('font-size', '20px');
+//     }
+// });
+
+//     // Festive Theme Mode
+// $('.festive').on('click', function(){
+//         $('#container').css('background-color', 'green');
+//         $('h1').css('color', 'red');
+//         $('h2').css('color', 'red');
+//         $('.info').css('color', 'white');
+//         $('article').css('color', 'white');
+//     });
+
+//     // Auxiliary functions
+// function resetColors(){
+//     $('#container').css('background-color', '#f3e4cf'); // Paper color 
+//     $('#container').css('color', 'black');
+// }
+
+// function resetFont(){
+//     $('#container').css('font-size', '16px');
+// }
+
+// function removeClasses(){
+//     $("#container").removeClass('darkMode');
+//     $("#container").removeClass('raMode');
+//     $("#container").removeClass('festiveMode');
+// }
+// });
